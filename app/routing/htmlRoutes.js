30d83fc6133e8b8a1bc74f08603914e3d590efbc
Tom <tsechrist@outlook.com>
2019-07-08 23:09:56 -0500

    Adds routing code in htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,18 +3,17 @@ var path = require('path');
 module.exports = function(app) {
 
   
-  app.get('/routing', function(request , response) {
-    response.send("Routing also works!");
-  });
-  
-  app.get('/signal', function(request , response) {
-    console.log(__dirname + "index.html")
-    response.sendFile(path.join(__dirname , "/../public/index.html"));
+  app.get('/survey', function(request , response) {
+    // console.log(__dirname + "index.html")
+    response.sendFile(path.join(__dirname , "/../public/survey.html"));
   });
   
   app.get('/' , function(request , response) {
-    response.send("This server works!");
+    response.sendFile(__dirname , "/../public/home.html");
   });
 
+  app.get('*',function (request, response) {
+    response.redirect('/');
+});
 
 };

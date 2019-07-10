8a21e4281675c79688f1b247215a9269259bff84
Tom <tsechrist@outlook.com>
2019-07-10 16:21:26 -0500

    Makes friend finder work.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,30 @@ module.exports = function(app) {
   });
   
   app.post('/api/friends', function(request , response) {
+
     var newFriend = request.body;
+    var difference = 0;
+    var lowestIndex = 0;
+    var lowestDifference = 0;
     
     console.log(newFriend);
+
+    friends.forEach(function(friend , index) {
+      for (i = 0 ; i < friend.scores.length; i++) {
+        difference += Math.abs(parseInt(friend.scores[i]) - parseInt(newFriend.scores[i]));
+        if (difference < lowestDifference) {
+          lowestDifference = difference;
+          lowestIndex = index;
+        }
+      }
+    });
+
+    console.log(`friends[${lowestIndex}]: ${JSON.stringify(friends[lowestIndex])}`);
+
+    friends.push(newFriend);
+
+    response.json(friends[lowestIndex]);
     
-    response.json("New friend: " + newFriend);
   });
 
 

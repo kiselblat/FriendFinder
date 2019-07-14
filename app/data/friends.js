5aa19d057fc1401329aa37995856b42e3b8c4bd9
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2019-07-14 17:10:14 -0500

    Adds friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,154 @@
 var friends = [
+  // TODO: Add friends
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Deep Thought",
+    "photo": "./app/data/images/deep-thought.png",
     "scores": [
-        5,
+        4,
+        3,
+        3,
+        2,
+        2,
         1,
-        4,
-        4,
-        5,
         1,
         2,
+        3,
+        1
+      ]
+  },{
+    "name": "Lieutenant Commander Data",
+    "photo": "./app/data/images/Data_2366.jpg",
+    "scores": [
+        3,
+        4,
+        4,
         5,
         4,
-        1
+        3,
+        5,
+        2,
+        4,
+        4
+      ]
+  },{
+    "name": "Gort",
+    "photo": "./app/data/images/Gort.jpg",
+    "scores": [
+        1,
+        5,
+        2,
+        1,
+        3,
+        1,
+        1,
+        4,
+        1,
+        5
+      ]
+  },{
+    "name": "HAL-9000",
+    "photo": "./app/data/images/HAL.jpg",
+    "scores": [
+        4,
+        3,
+        3,
+        2,
+        3,
+        1,
+        3,
+        3,
+        2,
+        4
+      ]
+  },{
+    "name": "Johnny Number 5",
+    "photo": "./app/data/images/Johnny_5.jpg",
+    "scores": [
+        4,
+        3,
+        5,
+        4,
+        3,
+        3,
+        4,
+        3,
+        5,
+        3
+      ]
+  },{
+    "name": "Marvin the Paranoid Android",
+    "photo": "./app/data/images/Marvin_(HHGG).jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        3,
+        1,
+        1,
+        1,
+        1,
+        3
+      ]
+  },{
+    "name": "Robby the Robot",
+    "photo": "./app/data/images/robby.jpg",
+    "scores": [
+        3,
+        3,
+        2,
+        2,
+        4,
+        2,
+        3,
+        3,
+        3,
+        4
+      ]
+  },{
+    "name": "Class M-3 Model B-9 General Utility Non-Theorizing Environmental Control Robot",
+    "photo": "./app/data/images/Robot.jpg",
+    "scores": [
+        4,
+        4,
+        3,
+        3,
+        4,
+        3,
+        3,
+        5,
+        5,
+        4
+      ]
+  },{
+    "name": "Rosie the Robot Maid",
+    "photo": "./app/data/images/rosie.jpg",
+    "scores": [
+        3,
+        4,
+        3,
+        3,
+        4,
+        3,
+        3,
+        2,
+        5,
+        4
+      ]
+  },{
+    "name": "Joshua, a.k.a. WOPR",
+    "photo": "./app/data/images/WOPR.jpg",
+    "scores": [
+        5,
+        2,
+        1,
+        1,
+        3,
+        1,
+        1,
+        3,
+        4,
+        3
       ]
   },
 ];
